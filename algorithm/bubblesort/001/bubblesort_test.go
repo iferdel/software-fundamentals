b4e167cd6efaf35cc1d91afd5ee94480c31a0460
iferdel <ignaferdel@gmail.com>
2025-03-01 17:28:29 -0300

    add: bubblesort test init

--- algorithm/bubblesort/001/bubblesort_test.go
+++ algorithm/bubblesort/001/bubblesort_test.go
@@ -1,1 +1,30 @@
 package bubblesort
+
+import "testing"
+
+func TestBubbleSort(t *testing.T) {
+	tests := map[string]struct {
+		slice   []int
+		want    []int
+		wantErr bool
+	}{
+
+		"correct sort": {
+			slice:   []int{2, 5, 1, 7, 8, 10, 4},
+			want:    []int{1, 2, 4, 5, 7, 8, 10},
+			wantErr: false,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := bubbleSort(tc.slice)
+			if (err != nil) != tc.wantErr {
+				t.Errorf("bubbleSort(%v): error = %v, wantErr %v", tc.slice, err, tc.wantErr)
+			}
+			if got != tc.want {
+				t.Errorf("bubbleSort(%v): got %v, want %v", tc.slice, got, tc.want)
+			}
+		})
+	}
+}
